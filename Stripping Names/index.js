78b66bcfcb01fcf5196e42542ab30f12d314c1c3
muhammadhasanali <hassanhamid2010@yahoo.com>
2024-04-19 19:03:02 +0500

    Stripping Names completed

---
+++ Stripping Names/index.js
@@ -0,0 +1,4 @@
+let person_name = "\t Hasan \n";
+console.log(person_name);
+console.log(person_name.trim());
+export {};
---
+++ Stripping Names/index.ts
@@ -0,0 +1,3 @@
+let person_name = "\t Hasan \n"
+console.log(person_name)
+console.log(person_name.trim())
