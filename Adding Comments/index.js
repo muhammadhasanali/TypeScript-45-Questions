e35225bf940d6c858d5727e2adffb5e4e634a742
muhammadhasanali <hassanhamid2010@yahoo.com>
2024-04-19 20:00:22 +0500

    Adding Comments completed

---
+++ Adding Comments/index.js
@@ -0,0 +1,8 @@
+//Syed Muhammad Hasan Ali Raza
+//19-4-2024
+//In this program I will Manipulation my name to three different cases like Upper, Lower and capital case.
+let userName = "Hasan";
+console.log(userName.toLowerCase());
+console.log(userName.toUpperCase());
+console.log(userName);
+export {};
