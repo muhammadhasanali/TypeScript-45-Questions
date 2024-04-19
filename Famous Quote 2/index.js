7c7fe0da552ba518625dcc7eb97b7e591d8ac50a
muhammadhasanali <hassanhamid2010@yahoo.com>
2024-04-19 18:37:06 +0500

    Famous Quote 2

---
+++ Famous Quote 2/index.js
@@ -0,0 +1,4 @@
+let famous_person = "Jensen Huang";
+let message = "Software is eating the world, but AI is going to eat software.";
+console.log(famous_person + " once said, " + '"' + message + '"');
+export {};
