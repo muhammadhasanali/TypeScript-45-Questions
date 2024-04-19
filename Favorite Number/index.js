cfc8f67d1fc2fe6da636bb48433b071bc8767011
muhammadhasanali <hassanhamid2010@yahoo.com>
2024-04-19 19:40:33 +0500

    Favorite Number completed

---
+++ Favorite Number/index.js
@@ -0,0 +1,3 @@
+let favoriteNumber = 555;
+console.log("I'm revealing my favorite Number " + favoriteNumber);
+export {};
---
+++ Favorite Number/index.ts
@@ -0,0 +1,2 @@
+let favoriteNumber = 555
+console.log("I'm revealing my favorite Number which is "+favoriteNumber)
