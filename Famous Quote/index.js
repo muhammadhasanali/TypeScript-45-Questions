d19eba64de6e7d48288fcbe95e684681643024ba
muhammadhasanali <hassanhamid2010@yahoo.com>
2024-04-19 18:08:28 +0500

    Famous Quote completed

---
+++ Famous Quote/index.js
@@ -0,0 +1,4 @@
+let author = "Jensen Huang";
+let quote = "Software is eating the world, but AI is going to eat software.";
+console.log(author + " once said, " + '"' + quote + '"');
+export {};
---
+++ Famous Quote/index.ts
@@ -0,0 +1,3 @@
+let author = "Jensen Huang"
+let quote = "Software is eating the world, but AI is going to eat software."
+console.log(author + " once said, " + '"'+quote+'"');
