4beec6dcdf0038b3a7336c14cc3d6b0942577bbf
muhammadhasanali <hassanhamid2010@yahoo.com>
2024-04-22 14:45:24 +0500

    Question 12 done.

---
+++ Greetings/index.js
@@ -0,0 +1,5 @@
+let names = ["Mujeeb", "Haseeb", "Noman"];
+console.log("Hello " + names[0] + ", How are you?");
+console.log(`Hello ${names[1]}, How are you?`);
+console.log("Hello " + names[2] + ", How are you?");
+export {};
---
+++ Greetings/index.ts
@@ -0,0 +1,4 @@
+let names = ["Mujeeb", "Haseeb", "Noman"]
+console.log("Hello " + names[0] + ", How are you?")
+console.log(`Hello ${names[1]}, How are you?`)
+console.log("Hello " + names[2] + ", How are you?")
