605f88308581cc8c2166693318906bd6af40fdbe
muhammadhasanali <hassanhamid2010@yahoo.com>
2024-04-22 16:29:57 +0500

    Question 14 done

---
+++ Guest List/index.js
@@ -0,0 +1,5 @@
+let guestlist = ["Mamu's Family", "Chacha's Family", "Cousin's Family"];
+for (let g = 0; g < guestlist.length; g++) {
+    console.log(`I would like to invite ${guestlist[g]} today for dinner at my home`);
+}
+export {};
