e9bcbd36b992accb4e91290d4ee0315932391bdf
muhammadhasanali <hassanhamid2010@yahoo.com>
2024-04-22 13:02:19 +0500

    Question 11: Names done.

---
+++ Names/index.js
@@ -0,0 +1,5 @@
+let names = ["Mujeeb", "Haseeb", "Noman"];
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+export {};
---
+++ Names/index.ts
@@ -0,0 +1,4 @@
+let names = ["Mujeeb", "Haseeb", "Noman"]
+console.log(names[0])
+console.log(names[1])
+console.log(names[2])
