fa07a89de6141b2ec32a673ec899dba6dd3e9d9c
muhammadhasanali <hassanhamid2010@yahoo.com>
2024-04-24 18:31:28 +0500

    Question 26 done

---
+++ Alien Colors #2/index.js
@@ -0,0 +1,8 @@
+let alien_color = "white";
+if (alien_color == "green") {
+    console.log("You have just earned 5 points for shooting the alien");
+}
+else {
+    console.log("You have just earned 10 points for shooting the alien");
+}
+export {};
