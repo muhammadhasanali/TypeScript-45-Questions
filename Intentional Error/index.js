37dc106547e138e12ee6ad24082e85377ad47827
muhammadhasanali <hassanhamid2010@yahoo.com>
2024-04-24 16:09:16 +0500

    Question 22 done

---
+++ Intentional Error/index.js
@@ -0,0 +1,5 @@
+//Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+let newarray = ["Apple", "Samsung", 10, "Google", "One Plus", 20];
+console.log(newarray[2]);
+console.log(newarray[9]);
+export {};
---
+++ Intentional Error/index.ts
@@ -0,0 +1,6 @@
+//Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+
+let newarray: (string | number)[] = ["Apple", "Samsung", 10, "Google", "One Plus", 20]
+
+console.log(newarray[2])
+console.log(newarray[9])
