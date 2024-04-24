eacb754a29ea66fa4a784a22295eea000a11ae5d
muhammadhasanali <hassanhamid2010@yahoo.com>
2024-04-24 19:27:32 +0500

    Question 29 done

---
+++ Favorite Fruit/index.ts
@@ -0,0 +1,14 @@
+let favorite_fruits = ["Mango", "Papaya", "Melon"]
+
+if (favorite_fruits.includes("Mango")) {
+    console.log(`I really like Mango`)
+}
+else if (favorite_fruits.includes("Papaya")) {
+    console.log(`I really like Papaya`)
+}
+else if (favorite_fruits.includes("Melon")) {
+    console.log(`I really like Melon`)
+}
+else {
+    console.log("No fruit! available.")
+}
