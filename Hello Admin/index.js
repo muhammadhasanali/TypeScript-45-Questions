bca5f3e3b82f4e1c2f9088970cfbccf00d838280
muhammadhasanali <hassanhamid2010@yahoo.com>
2024-04-24 19:36:52 +0500

    Question 30 done

---
+++ Hello Admin/index.js
@@ -0,0 +1,10 @@
+let user = ["Hasan", "Haseeb", "Admin", "Kamal", "Waqas", "Waqar"];
+for (let i = 0; i < user.length; i++) {
+    if (user[i] == "Admin") {
+        console.log(`Hello ${user[i]}, would you like to see a status report?`);
+    }
+    else {
+        console.log(`Hello ${user[i]}, thank you for logging in again.`);
+    }
+}
+export {};
