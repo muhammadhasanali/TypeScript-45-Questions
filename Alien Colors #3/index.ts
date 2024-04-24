3be1b6cbfb16a836811f021aeead0e0207ccec30
muhammadhasanali <hassanhamid2010@yahoo.com>
2024-04-24 18:45:48 +0500

    Question 27 done

---
+++ Alien Colors #3/index.ts
@@ -0,0 +1,11 @@
+let alien_color = "green"
+let alien_color2 = "yellow"
+let alien_color3 = "red"
+
+if (alien_color == "green"){
+    console.log("You have earned 5 points")
+} if (alien_color2 == "yellow") {
+    console.log("You have earned 10 points")
+} if (alien_color3 == "red"){
+    console.log("You have earned 15 points")
+}
