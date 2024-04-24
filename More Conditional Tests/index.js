813b05a02d3589d2c8d854bbb911c7dc78540d93
muhammadhasanali <hassanhamid2010@yahoo.com>
2024-04-24 18:06:37 +0500

    Question 24 done

---
+++ More Conditional Tests/index.js
@@ -0,0 +1,27 @@
+// Tests for equality and inequality with strings
+let myname = "Hasan";
+console.log(myname == "Hasan");
+console.log(myname != "hasan");
+// Tests using the lower case function
+console.log(myname.toLowerCase() == "hasan");
+// Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
+let mynumber = 5;
+console.log(mynumber == 5);
+console.log(mynumber != 4);
+console.log("\n");
+console.log(mynumber > 4);
+console.log(mynumber < 4);
+console.log("\n");
+console.log(mynumber >= 4);
+console.log(mynumber <= 4);
+console.log("\n");
+// Tests using "and" and "or" operators
+console.log(myname == "Hasan" && mynumber == 5);
+console.log(myname == "Hasan" || mynumber == 4);
+console.log(myname == "hasan" || mynumber == 4);
+console.log("\n");
+// Test whether an item is in a array
+let myarray = ["Hasan", "Haseeb", "Ali"];
+console.log(myarray[0] == "Hasan");
+console.log(myarray[1] == "Ali");
+export {};
