7e1f895ba917ca9e207231059c2fb50551904c0b
muhammadhasanali <hassanhamid2010@yahoo.com>
2024-04-24 18:26:19 +0500

    Question 25 done

---
+++ Alien Colors #1/index.js
@@ -0,0 +1,8 @@
+let alien_color = "blue";
+if (alien_color == "green") {
+    console.log("You have just won 5 points");
+}
+if (alien_color == "red") {
+    console.log("You have just won 5 points");
+}
+export {};
---
+++ Alien Colors #1/index.ts
@@ -0,0 +1,10 @@
+let alien_color = "green"
+
+if (alien_color == "green"){
+    console.log("You have just won 5 points")
+}
+
+
+if (alien_color == "red"){
+    console.log("You have just won 5 points")
+}
