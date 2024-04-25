7ef909bf688f7fb47ff113efe70599749ae3c886
muhammadhasanali <hassanhamid2010@yahoo.com>
2024-04-25 18:44:07 +0500

    Question 36 done

---
+++ T-Shirt/index.js
@@ -0,0 +1,9 @@
+// function make_shirt(size: number, message:string){
+//     return `Your T-Shirt size is ${size} and T-shirt print message is "${message}"`
+// }
+// console.log(make_shirt(32, "Life begins at the end of your comfort zone."))
+let mything = (size, message) => {
+    return `Your T-Shirt size is ${size} and T-shirt print message is "${message}"`;
+};
+console.log(mything(32, "Life begins at the end of your comfort zone."));
+export {};
---
+++ T-Shirt/index.ts
@@ -0,0 +1,13 @@
+// function make_shirt(size: number, message:string){
+//     return `Your T-Shirt size is ${size} and T-shirt print message is "${message}"`
+// }
+
+// console.log(make_shirt(32, "Life begins at the end of your comfort zone."))
+
+
+
+let make_shirt = (size: number, message:string)=>{
+    return `Your T-Shirt size is ${size} and T-shirt print message is "${message}"`
+}
+
+console.log(make_shirt(32,"Life begins at the end of your comfort zone."))
