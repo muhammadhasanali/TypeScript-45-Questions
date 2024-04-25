ea5959dd7d290e2d47a87245d1a48cc2c9469e72
muhammadhasanali <hassanhamid2010@yahoo.com>
2024-04-25 20:23:00 +0500

    question 40 done

---
+++ Album/index.js
@@ -0,0 +1,7 @@
+let make_album = (artistName, albumTitle, tracks) => {
+    return { artistName, albumTitle, tracks };
+};
+console.log(make_album("Hasan", "The Bad Boy", 10));
+console.log(make_album("Ali", "Crazy Days", 5));
+console.log(make_album("Mudasir", "Hidden jokes"));
+export {};
