62764a5380bfe486c8894cdf7753d2c4a7a15de3
muhammadhasanali <hassanhamid2010@yahoo.com>
2024-04-25 18:08:41 +0500

    Question 35 done

---
+++ Animals/index.js
@@ -0,0 +1,5 @@
+let animal = ["Kutta", "Khurram", "Billi", "Jagha"];
+for (let i = 0; i < animal.length; i++) {
+    console.log(`${animal[i]} would make a great pet`);
+}
+export {};
---
+++ Animals/index.ts
@@ -0,0 +1,6 @@
+let animal = ["Dog","Cow","Cat","Goat"]
+
+for (let i = 0; i < animal.length; i++) {
+    console.log(`${animal[i]} would make a great pet`)
+}
+
