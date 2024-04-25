4abb5a11614eb90b841a3360b57ed173a96d8fb7
muhammadhasanali <hassanhamid2010@yahoo.com>
2024-04-25 17:13:56 +0500

    Question 34 done

---
+++ Pizzas/index.js
@@ -0,0 +1,5 @@
+let pizzas = ["Fajita", "Tikka", "Arabian"];
+for (let i = 0; i < pizzas.length; i++) {
+    console.log(pizzas[i]);
+}
+export {};
---
+++ Pizzas/index.ts
@@ -0,0 +1,7 @@
+let pizzas = ["Fajita","Tikka","Arabian"]
+
+for (let i = 0; i < pizzas.length; i++){
+    console.log(`I really like ${pizzas[i]} Pizza`)
+}
+
+console.log("\n I really like to eat Pizzas. Pizza comes in a vareity of flavors and toopings, allowing individuals to customize it to their liking")
