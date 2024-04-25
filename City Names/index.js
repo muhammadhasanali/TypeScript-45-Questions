2e664afa5526d7221feb8555dddf92abc455a25a
muhammadhasanali <hassanhamid2010@yahoo.com>
2024-04-25 19:54:01 +0500

    Question 39 done

---
+++ City Names/index.js
@@ -0,0 +1,7 @@
+let city_country = (city, country) => {
+    return `"${city}, ${country}"`;
+};
+console.log(city_country("Karachi", "Pakistan"));
+console.log(city_country("Lahore", "Pakistan"));
+console.log(city_country("Faislabad", "Pakistan"));
+export {};
