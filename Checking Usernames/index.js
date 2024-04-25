cb05de96df5ce17534711dccdf00f9df6bed7de6
muhammadhasanali <hassanhamid2010@yahoo.com>
2024-04-25 16:52:37 +0500

    Question 32 done

---
+++ Checking Usernames/index.js
@@ -0,0 +1,28 @@
+let current_users = ["Ali", "Hasan", "Haseeb", "Kamal", "Mujeeb"];
+let new_users = ["Mubeen", "Saad", "Saud", "ALI", "Munna", "Raju"];
+new_users.forEach(users => {
+    let users_cas = users.toLowerCase();
+    let usernameTaken = current_users.some(c_user => c_user.toLowerCase() === users_cas);
+    if (usernameTaken) {
+        console.log(`${users} needs to choose a new username because it's already taken`);
+    }
+    else {
+        current_users.push(users);
+        console.log(`${users} is the new member added`);
+    }
+});
+console.log(current_users);
+current_users = [];
+new_users.forEach(users => {
+    let users_cas = users.toLowerCase();
+    let usernameTaken = current_users.some(c_user => c_user.toLowerCase() === users_cas);
+    if (usernameTaken) {
+        console.log(`${users} needs to choose a new username because it's already taken`);
+    }
+    else {
+        current_users.push(users);
+        console.log(`${users} is the new member added`);
+    }
+});
+console.log(current_users);
+export {};
