0932130533a99c4248deea2cb6274a626f355cfb
muhammadhasanali <hassanhamid2010@yahoo.com>
2024-04-29 14:55:32 +0500

    Question 41 done

---
+++ Magicians/index.js
@@ -0,0 +1,8 @@
+let Magicians = ["ABC", "DEF", "GHI", "JKL"];
+function show_magicians() {
+    for (let Magician of Magicians) {
+        console.log(Magician);
+    }
+}
+show_magicians();
+export {};
---
+++ Magicians/index.ts
@@ -0,0 +1,9 @@
+let Magicians = ["ABC","DEF","GHI","JKL"]
+
+function show_magicians(){
+    for (let Magician of Magicians){
+        console.log(Magician)
+    }
+}
+
+show_magicians()
